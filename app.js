7005f3f72dd4231fd15f2d685880d36650501fef
Aerobird98 <none>
2017-02-05 12:51:39 +0100

    updated electron, and new style

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const electron = require('electron')
 const {app, BrowserWindow} = electron
+const path = require('path')
+const url = require('url')
 
 let win
 
 function createWindow () {
   win = new BrowserWindow({width: 800, height: 600, title: 'Notes'})
-  win.setMenu(null)
-  win.loadURL(`file://${__dirname}/index.html`)
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
   win.on('closed', () => {
     win = null
   })
@@ -16,3 +21,4 @@ app.on('ready', createWindow)
 app.on('window-all-closed', () => {
   app.quit()
 })
+
